fcfe46c95010b50389a66e146d9d42e30e30b072
Patrick M. Menzel <pmmenzel@gmail.com>
2015-02-20 22:52:48 -0800

    update accounts controller and some view logic

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -3,15 +3,26 @@ class AccountsController < ApplicationController
 	include AccountsHelper
 	include Databasedotcom::Rails::Controller
 
-  def index
+
+	def index
   	# In tutorial, they suggest doing it this way but I get a SalesForceError
   	# @accounts = Account.query("Id != NULL LIMIT 20")
   	@accounts = Account.all()[0..19]
-  	lookup
+
   end
 
   def create
-  	@account = Account.all.include?(params[:account])
+  	# account = Account.all.include?(params[:account])
+
+  	# account = Account.new(params[:account])
+  	# account.Name = false
+  	# account.IsReminderSet = false
+  	# account.Priority = "Normal"
+  	# user = User.first
+  	# account.OwnerId = user.Id
+  	# if (account.save)
+  	# 	redirect_to(account, :notice => 'Task was successfully created.')
+  	# end
   end
 
   def show
--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -2,11 +2,21 @@ require "clearbit"
 
 module AccountsHelper
 
+	def find_or_create
+		user = User.find(params[:id])
+		p user.company
+	end
+
 	def lookup
-		person = Clearbit::Person.find(email: 'efrank@genepoint.com')
+		person = Clearbit::Person.find(email: 'steven@devbootcamp.com')
 
 		if person && !person.pending?
-			puts "Name: #{person.name.fullName} Company: #{person.company}"
+			# p "About to print steve's company name"
+			# p person.company.name
+			# p "printing steve's location"
+			# p person.location
+			# p "trying to print Company location"
+			# p person.company.location
 		end
 	end
 
