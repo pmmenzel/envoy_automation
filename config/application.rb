133e079b3a44d91a03ecdee08a96f3f3bd885e17
Patrick M. Menzel <pmmenzel@gmail.com>
2015-02-22 15:09:59 -0800

    configure new registration views with devise

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,9 @@ Bundler.require(*Rails.groups)
 module EnvoyAutomation
   class Application < Rails::Application
     config.autoload_paths += %W(#{config.root}/lib)
+
+    # config.paths['app/views'] << "app/views/devise"
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
