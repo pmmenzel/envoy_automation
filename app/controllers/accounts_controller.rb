7a43d00f0b055fc90a6d29253628705ecbf31b07
Patrick M. Menzel <pmmenzel@gmail.com>
2015-02-22 15:03:57 -0800

    add conditional to check if user already exists as a contact on salesforce

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,28 +1,17 @@
 class AccountsController < ApplicationController
-	# require_relative "../../lib/clearbit_lookup.rb"
-	include AccountsHelper
+	include ApplicationHelper
 	include Databasedotcom::Rails::Controller
 
 
 	def index
-  	# In tutorial, they suggest doing it this way but I get a SalesForceError
-  	# @accounts = Account.query("Id != NULL LIMIT 20")
-  	@accounts = Account.all()[0..19]
-
+    @accounts = Account.all()[0..19]
+    if !user_exists?
+      sf_create_contact_and_oppoortunity
+    end
   end
 
   def create
-  	# account = Account.all.include?(params[:account])
 
-  	# account = Account.new(params[:account])
-  	# account.Name = false
-  	# account.IsReminderSet = false
-  	# account.Priority = "Normal"
-  	# user = User.first
-  	# account.OwnerId = user.Id
-  	# if (account.save)
-  	# 	redirect_to(account, :notice => 'Task was successfully created.')
-  	# end
   end
 
   def show
